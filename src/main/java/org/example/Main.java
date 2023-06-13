636d959dffed89b3db3b837cc1db6221f3b4d928
Bormental61 <tyranosaurus@yandex.ru>
2023-06-13 16:05:41 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        BinarTree<Integer> tree = new BinarTree<>();
+
+        tree.add(1);
+        tree.add(2);
+        tree.add(3);
+        tree.add(4);
+        tree.add(5);
+        tree.add(6);
+        tree.add(7);
+        tree.add(8);
+        tree.add(9);
+        tree.add(10);
+        tree.add(11);
+        tree.add(12);
+        tree.add(13);
+        tree.add(14);
+        tree.add(15);
+        tree.print();
+    }
+}
